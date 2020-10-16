125ecdc1a1210b6cad438fc3cb7e31f860920462
Renan Zapelini <renanzapelini27@gmail.com>
2020-10-16 18:01:29 -0300

    criado endpoints para consulta no dashboard de quantidades do dia #14

--- src/main/java/br/com/zapelini/lanzendorf/facialrecognitionapi/repository/aula/AulaRepository.java
+++ src/main/java/br/com/zapelini/lanzendorf/facialrecognitionapi/repository/aula/AulaRepository.java
@@ -3,6 +3,7 @@ package br.com.zapelini.lanzendorf.facialrecognitionapi.repository.aula;
 import br.com.zapelini.lanzendorf.facialrecognitionapi.model.Aula;
 import br.com.zapelini.lanzendorf.facialrecognitionapi.model.Turma;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +12,8 @@ import java.util.List;
 public interface AulaRepository extends JpaRepository<Aula, Long> {
 
     List<Aula> findByTurma(Turma turma);
+
+    @Query("SELECT count(a) FROM Aula a WHERE to_date(to_char(inicio, 'YYYY/MM/DD'), 'YYYY/MM/DD') = current_date")
+    Long aulasdoDia();
 
 }
